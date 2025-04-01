b5902e8c254af553644c59150f46eeed0792d438
alexander kusakin <pm12.kusakin@gmail.com>
2025-04-01 18:16:56 +0700

    ./internal/config/logger/: add logger config

---
+++ internal/config/logger/logger.go
@@ -0,0 +1,26 @@
+package loggerconfig
+
+import (
+	"io"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func Init() {
+
+	file, err := os.OpenFile("Logger.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Failed to open log file: %v", err)
+	}
+
+	multiWriter := io.MultiWriter(os.Stdout, file)
+
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+		ForceColors:     true,
+	})
+
+	log.SetOutput(multiWriter)
+}
