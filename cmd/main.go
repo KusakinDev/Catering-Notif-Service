0cf618e26e31742be0ff11122243b99318626df4
alexander kusakin <pm12.kusakin@gmail.com>
2025-04-01 16:43:08 +0700

    all proj: refactor

--- cmd/main.go
+++ cmd/main.go
@@ -12,18 +12,15 @@ package main
 import (
 	"log"
 
-	// WARNING!
-	// Pass --git-repo-id and --git-user-id properties when generating the code
-	//
-	sw "github.com/GIT_USER_ID/GIT_REPO_ID/go"
+	routerpkg "github.com/KusakinDev/Catering-Notif-Service/internal/routes"
 )
 
 func main() {
-	routes := sw.ApiHandleFunctions{}
+	routes := routerpkg.ApiHandleFunctions{}
 
 	log.Printf("Server started")
 
-	router := sw.NewRouter(routes)
+	router := routerpkg.NewRouter(routes)
 
 	log.Fatal(router.Run(":8080"))
 }
---
+++ internal/database/db.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DataBase struct {
+	Connection *gorm.DB
+}
+
+func (database *DataBase) InitDB() error {
+	var err error
+	dsn := "host=localhost user=postgres password=5121508 dbname=catering_notif_db port=5432 sslmode=disable"
+	database.Connection, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Println("Error open DB connection: ", err)
+		return err
+	}
+	return nil
+}
+
+func (database *DataBase) CloseDB() error {
+	sqlDB, err := database.Connection.DB()
+	if err != nil {
+		log.Println("Error close DB connection: ", err)
+		return err
+	}
+	sqlDB.Close()
+	return nil
+}
